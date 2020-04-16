d2ed0a4e782e95566c28a18f710e37b3cdf5f29d
Ariful islam <55908839+Ariful0007@users.noreply.github.com>
2020-04-15 20:16:31 -0700

    Create Reverse Coding.py

---
+++ Reverse Coding.py
@@ -0,0 +1,14 @@
+for _ in range(int(input())):
+    n= int(input())
+    m= int(input())
+    sum=0
+    for i in range(1,n+1):
+        sum=sum+i
+
+
+    if sum==m:
+        print("1")
+    else :
+        print("0")
+if __name__ == '__main__':
+    pass
